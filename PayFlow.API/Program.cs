f48ec6cb679da67635d42b987d4f9cc1582ffc71
EdgarSC <edgar13j13j13j@gmail.com>
2025-05-14 23:34:29 -0500

    Agregar archivos de proyecto.

---
+++ PayFlow.API/Program.cs
@@ -0,0 +1,41 @@
+using Microsoft.EntityFrameworkCore;
+using PayFlow.DOMAIN.Infrastructure.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+var config = builder.Configuration;
+var connectionString = config.GetConnectionString("DevConnection");
+builder.Services.AddDbContext<PayflowContext>(options =>
+    options.UseSqlServer(connectionString));
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+//Add swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+    {
+        Title = "PayFlow API",
+        Version = "v1"
+    });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
