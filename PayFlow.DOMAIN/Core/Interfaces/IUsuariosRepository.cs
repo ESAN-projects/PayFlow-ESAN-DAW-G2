f84263cfbfb1fd1ac430a5568a4d0941cb1c47dd
EdgarSC <edgar13j13j13j@gmail.com>
2025-05-15 21:57:06 -0500

    Se termina usuarios

---
+++ PayFlow.DOMAIN/Core/Interfaces/IUsuariosRepository.cs
@@ -0,0 +1,13 @@
+﻿using PayFlow.DOMAIN.Core.Entities;
+
+namespace PayFlow.DOMAIN.Core.Interfaces
+{
+    public interface IUsuariosRepository
+    {
+        Task<int> AddUsuarioAsync(Usuarios usuario);
+        Task<bool> DeleteUsuarioAsync(int id);
+        Task<IEnumerable<Usuarios>> GetAllUsuariosAsync();
+        Task<Usuarios?> GetUsuarioByIdAsync(int id);
+        Task<bool> UpdateUsuarioAsync(Usuarios usuario);
+    }
+}
