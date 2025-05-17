1adfd2b50d9cc27ad88b158fed8910c081b8c5ca
DNA <23100081@ue.edu.pe>
2025-05-16 21:18:27 -0500

    Faltarian Uodate, Delete x2

--- PayFlow.API/Controllers/AdministradoresController.cs
+++ PayFlow.API/Controllers/AdministradoresController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using PayFlow.DOMAIN.Infrastructure.Data;
 using PayFlow.DOMAIN.Core.Interfaces;
+using PayFlow.DOMAIN.Core.Entities;
 
 namespace PayFlow.API.Controllers
 {
@@ -22,6 +23,29 @@ namespace PayFlow.API.Controllers
         {
             var administradores = await _administradoresRepository.GetAllAdministradoresAsync();    
             return Ok(administradores);
+        }
+
+        // GET Administrador by ID
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetAdministradoresById(int id)
+        {
+            var administradores = await _administradoresRepository.GetAdministradoresByIdAsync(id);
+            if (administradores == null)
+            {
+                return NotFound();
+            }
+            return Ok(administradores);
+        }
+        // POST add Administradores
+        [HttpPost]
+        public async Task<IActionResult> AddAdministradores([FromBody] Administradores administradores)
+        {
+            if (administradores == null)
+            {
+                return BadRequest();
+            }
+            var AdministradoresId = await _administradoresRepository.AddAdministradoresAsync(administradores);
+            return CreatedAtAction(nameof(GetAdministradoresById), new { id = AdministradoresId }, administradores);
         }
     }
 }
